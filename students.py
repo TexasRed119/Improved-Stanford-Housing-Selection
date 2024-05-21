568b7c496137857ccaf0743b5e5e2744bc78eed9
Ismail Mardin <ikmardin@stanford.edu>
2024-05-21 04:25:49 -0700

    Adding modified students.py to make current_mech work

--- students.py
+++ students.py
@@ -3,6 +3,8 @@ import json
 import numpy as np
 import pandas as pd
 from modified_srsd import modified_srsd
+from modified_srsd_multipleOAE import multipleOAE_modified_srsd
+
 
 with open('original.json', 'r') as file:
     rooms_data = json.load(file)
@@ -40,153 +42,178 @@ TODO: We currently have not included room configurations in our ranking system (
 TODO: how to watch for room configurations and housing groups (how does this work?)
 '''
 def generate_students(rooms_data, num_students, dorm_names, room_configurations, accomodations):
-  students = []
+    students = []
+    column_names = ["student_id", "year", "OAE", "Assignment", "Rankings"]
+    column_names.extend(dorm_names)
 
-  column_names = ["student_id", "year", "OAE", "Assignment"]
-  for dorm in dorm_names:
-    column_names.append(dorm)
+    for i in range(num_students):
+        current_student = [None for u in range(len(column_names))]
 
-  for i in range(num_students):
-    current_student = [None for u in range(len(column_names))]
+        # student id
+        current_student[0] = i
 
-    #student id
-    current_student[0] = i
+        # student upcoming year at stanford
+        year = random.randint(2, 4)
+        current_student[1] = year
 
-    #student upcoming year at stanford
-    year = random.randint(2,4)
-    current_student[1] = year
+        # OAE accommodations
+        num_oae = random.choices([0, 1, 2], weights=[0.6, 0.25, 0.15])[0]
+        oae_accommodations = random.sample(accomodations[:-1], num_oae)
+        current_student[2] = ",".join(oae_accommodations) if num_oae > 0 else "None"
 
-    #OAE type
-    #can add more columns for multiple OAE in future (e.g. "OAE_1", "OAE_2") and repeat process below
-    #TODO: multiple OAEs
-    r = random.randint(1,10)
-    if (r <= 2): #for now set at 20% chance of having OAE, each type of OAE having equal probability of occuring
-      q = random.randint(0,6)
-      OAE_type = accomodations[q]
-      current_student[2] = OAE_type
-    else:
-      current_student[2] = "None"
+        # dorm rankings
+        suitable_dorms = []
+        for dorm in dorm_names:
+            if dorm in rooms_data:
+                if current_student[2] == "None":  # student has no OAE
+                    suitable_dorms.append(dorm)
+                else:  # student has OAE accommodations
+                    for room in room_configurations:
+                        if room in rooms_data[dorm]:
+                            for k in range(len(rooms_data[dorm][room])):
+                                if all(oae in rooms_data[dorm][room][k]["facilities"] for oae in oae_accommodations):
+                                    suitable_dorms.append(dorm)
+                                    break
+                        if dorm in suitable_dorms:
+                            break
 
-    '''
-    Different way of things I tried, ignore for now. Left in case we want to use in future implementation
+        # Generate rankings for suitable dorms
+        rankings = {}
+        for dorm in suitable_dorms:
+            rankings[dorm] = random.randint(1, len(suitable_dorms))
 
-    #below for loop randomly generates OAE accommodation types for current student
-    #note: we are currently capping # of OAE accomodations at 1 for now, will likely change and become more possible complex combinations of OAE types
-    num_OAE = 0
-    for oae in accomodations:
-      if (oae is not "None"): #not including "None" since would be perfectly collinear with all other OAE booleans being False"
-        column_names.append(oae)
-        if (num_OAE == 0):
-          r = random.randint(0, 19) # 20 possible outcomes
-          if (r == 0): #give 5% chance of having each OAE type, with 7 different types (currently) results in probability of student having an OAE at 35%
-            current_student.append(True)
-            num_OAE += 1
-          else:
-            current_student.append(False)
-        else:
-          current_student.append(False)
-    '''
+        # Sort the rankings based on the values
+        sorted_rankings = sorted(rankings.items(), key=lambda x: x[1])
 
-    #dorm rankings, first finding dorms that are suitable (i.e. must have facilties for accomodation of student)
-    suitable_dorms = []
-    for dorm in dorm_names:
-      if dorm in rooms_data:
-        if current_student[2] == "None": #student has no OAE
-          suitable_dorms.append(dorm)
-        else: #student does have an OAE
-          for room in room_configurations:
-            if room in rooms_data[dorm]:
-              for k in range(len(rooms_data[dorm][room])):
-                if current_student[2] in rooms_data[dorm][room][k]["facilities"]:  #would need to change index of current_student based on implementation above
-                  suitable_dorms.append(dorm)
-                  break
-              break
+        # Create a list of ranked dorms
+        ranked_dorms = [dorm for dorm, _ in sorted_rankings]
 
-    #now do random ranking TODO: more popular housing would be ranked higher (could make system based off of amenities provided, or do subjective ranking system for now)
-    #assign each house random ranking
-    suitable_dorms_randomized = random.sample(suitable_dorms, len(suitable_dorms))
-    #assign column with name suitable_dorms_randomized[i] with rank i+1 (rank will be 1-indexed)
-    for j in range(len(suitable_dorms_randomized)):
-      dorm = suitable_dorms_randomized[j]
-      dorm_index = column_names.index(dorm)
-      current_student[dorm_index] = j + 1
+        # Assign the ranked dorms to the current student
+        current_student[4] = ranked_dorms
 
-    students.append(current_student)
+        # Populate the individual dorm ranking columns
+        for dorm in dorm_names:
+            if dorm in ranked_dorms:
+                dorm_index = column_names.index(dorm)
+                current_student[dorm_index] = ranked_dorms.index(dorm) + 1
+            else:
+                dorm_index = column_names.index(dorm)
+                current_student[dorm_index] = None
 
-  return pd.DataFrame(data = students, columns = column_names)
+        students.append(current_student)
+
+    return pd.DataFrame(data=students, columns=column_names)
 
 
 '''
 serial dictatorship modeling current housing mechanism employed by stanford
 '''
-def current_assignment_mech(df_students, assignment_data):
-  #shuffle dataframe of students (this isn't really neccessary per se since data was random to begin with, but we will do for now anyways, may help when we incorporate houing groups later)
-  df_students = df_students.sample(frac = 1).reset_index(drop=True)
+def current_assignment_mech(df_students, rooms_data, year_priority):
+    # Shuffle dataframe of students
+    df_students = df_students.sample(frac=1).reset_index(drop=True)
 
+    # Sort students based on the specified year priority
+    df_students['year_priority'] = df_students['year'].map(lambda x: year_priority.index(x))
+    df_students.sort_values(by=['year_priority', 'student_id'], inplace=True)
 
-  #assign OAE students first for control/current system
-  #OAE assumption system for current assignment: 50% get first assignment, 50% get second
-  for year in range(4, 1, -1):
-    df_year = df_students.loc[df_students["year"] == year].reset_index(drop=True)
-    for i in range(len(df_year.index)):
-      student = df_year.loc[i,:]
-      if student['OAE'] != "None":
-        OAE = student['OAE']
-        r = random.randint(1,4)
-        #assign to top choice 50% of time (assumption for now)
-        assigned = False
-        k = 1
-        #TODO: what if only available choice for OAE is 1st choice? below starting at 2nd choice will cause bug. add clause to if statement? or maybe add something to for loop that loops back to 1 if no others after all options exhausted
+    # Initialize variables to store the housing assignments
+    assignments = {}
+
+    # Assign OAE students first for control/current system
+    # OAE assumption system for current assignment: 50% get first assignment, 50% get second
+    for _, student in df_students[df_students['OAE'] != "None"].iterrows():
+        student_id = student['student_id']
+        oae_accommodations = student['OAE'].split(',')
+        ranked_dorms = student['Rankings']
+        r = random.randint(1, 4)
+        # Assign to top choice 50% of time (assumption for now)
         if r > 2:
-          k = 2
-        #TODO: does below need to be changed to reflect when rank k choice doesn't exist to prevent out-of-bounds error? (this shouldn't occur ever tho, everyone can be assigned based on nature of system)
-        while assigned == False:
-          top_choice = student[student == k].index[-1]
-          #note that the below will look different when we incorporate room configurations in rankings
-          for room_type in assignment_data[top_choice]:
-            for j in range(len(assignment_data[top_choice][room_type])):
-              if OAE in assignment_data[top_choice][room_type][j]['facilities']:
-                if assignment_data[top_choice][room_type][j]['num_rooms'] > 0:
-                  df_students.loc[df_students['student_id'] == student['student_id'], "Assignment"] = top_choice
-                  assignment_data[top_choice][room_type][j]['num_rooms'] = assignment_data[top_choice][room_type][j]['num_rooms'] - 1
-                  assigned = True
-                  break
-          k += 1
+            ranked_dorms = ranked_dorms[1:]  # Remove the first choice
 
-  #now traverse and assign through each year, starting with seniors
-  for year in range(4, 1, -1):
-    df_year = df_students.loc[df_students["year"] == year].reset_index(drop=True)
-    for i in range(len(df_year.index)):
-      student = df_year.loc[i,:]
-      if student['OAE'] == "None": #non-OAE students only, OAE already assigned
         assigned = False
-        k = 1
-        while assigned == False: #TODO: does this need to be changed to reflect when rank k choice doesn't exist to prevent out-of-bounds error? (this shouldn't occur ever tho, everyone can be assigned based on nature of system)
-          top_choice = student[student == k].index[-1]
-          #note that the below will look different when we incorporate room configurations in rankings
-          for room_type in assignment_data[top_choice]:
-            for j in range(len(assignment_data[top_choice][room_type])):
-              if assignment_data[top_choice][room_type][j]['num_rooms'] > 0:
-                df_students.loc[df_students['student_id'] == student['student_id'], "Assignment"] = top_choice
-                assignment_data[top_choice][room_type][j]['num_rooms'] = assignment_data[top_choice][room_type][j]['num_rooms'] - 1
-                assigned = True
+        for dorm in ranked_dorms:
+            if dorm in rooms_data:
+                for room_type in rooms_data[dorm]:
+                    for j in range(len(rooms_data[dorm][room_type])):
+                        if all(oae in rooms_data[dorm][room_type][j]['facilities'] for oae in oae_accommodations):
+                            if rooms_data[dorm][room_type][j]['num_rooms'] > 0:
+                                assignments[student_id] = (dorm, room_type)
+                                rooms_data[dorm][room_type][j]['num_rooms'] -= 1
+                                assigned = True
+                                break
+                    if assigned:
+                        break
+            if assigned:
                 break
-          k += 1
+        if not assigned:
+            # If no suitable room found, assign to any available room
+            for dorm in rooms_data:
+                for room_type in rooms_data[dorm]:
+                    for j in range(len(rooms_data[dorm][room_type])):
+                        if rooms_data[dorm][room_type][j]['num_rooms'] > 0:
+                            assignments[student_id] = (dorm, room_type)
+                            rooms_data[dorm][room_type][j]['num_rooms'] -= 1
+                            assigned = True
+                            break
+                    if assigned:
+                        break
+                if assigned:
+                    break
 
-  return df_students, assignment_data
+    # Now traverse and assign through each year, based on the year priority
+    for _, student in df_students[df_students['OAE'] == "None"].iterrows():
+        student_id = student['student_id']
+        ranked_dorms = student['Rankings']
+        assigned = False
+        for dorm in ranked_dorms:
+            if dorm in rooms_data:
+                for room_type in rooms_data[dorm]:
+                    for j in range(len(rooms_data[dorm][room_type])):
+                        if rooms_data[dorm][room_type][j]['num_rooms'] > 0:
+                            assignments[student_id] = (dorm, room_type)
+                            rooms_data[dorm][room_type][j]['num_rooms'] -= 1
+                            assigned = True
+                            break
+                    if assigned:
+                        break
+            if assigned:
+                break
+        if not assigned:
+            # If no suitable room found, assign to any available room
+            for dorm in rooms_data:
+                for room_type in rooms_data[dorm]:
+                    for j in range(len(rooms_data[dorm][room_type])):
+                        if rooms_data[dorm][room_type][j]['num_rooms'] > 0:
+                            assignments[student_id] = (dorm, room_type)
+                            rooms_data[dorm][room_type][j]['num_rooms'] -= 1
+                            assigned = True
+                            break
+                    if assigned:
+                        break
+                if assigned:
+                    break
+
+    return assignments
 
 df_students = generate_students(rooms_data, 90, dorm_names, room_configurations, accomodations)
 
 # print(df_students)
 
 # Example usage
-oae_threshold = 0.3  # Adjust the threshold as needed
+# oae_threshold = 0.3  # Adjust the threshold as needed
 year_priority = [4, 3, 2]  # Specify the year priority order
 
-assignments = modified_srsd(df_students, rooms_data, oae_threshold, year_priority)
+modified_assignments = multipleOAE_modified_srsd(df_students, rooms_data, year_priority)
+current_assignments = current_assignment_mech(df_students, rooms_data, year_priority)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(df_students)
 
-print(df_students)
-
-# Print the housing assignments
-for student_id, (dorm, room) in assignments.items():
+print("Current mech:")
+for student_id, (dorm, room) in current_assignments.items():
     print(f"Student {student_id}: {dorm} - {room}")
+
+print("Modified mech:")
+for student_id, (dorm, room) in modified_assignments.items():
+    print(f"Student {student_id}: {dorm} - {room}")
+
+
